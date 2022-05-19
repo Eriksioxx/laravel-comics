c35894735ab876df45e9a148484dafb12e6eb455
erik schievenin <erik.schievenin@gmail.com>
2022-05-19 18:31:14 +0200

    Exercise complete

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,14 +9,15 @@
     <title>@yield('pageTitle')</title>
     <script src="{{ asset('js/app.js') }}"></script>
 </head>
+
 <body>
     <header class="header">
         {{-- uso la stessa navbar per tutte le pagine --}}
         @include('partials.navbar')
     </header>
-    
+
     <main>
-        @yield('mainContent') 
+        @yield('mainContent')
     </main>
 
     <footer>
@@ -23,4 +25,5 @@
         @include('partials.footer')
     </footer>
 </body>
+
 </html>
